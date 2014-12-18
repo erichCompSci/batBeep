11ed86d065a7f47a54bbe41b527178221283a910
Erich Lohrmann <erichlohrmann@gmail.com>
2014-12-17 22:59:43 -0500

    Simple Functioning/Testing Version, no bells and whistles yet

--- batbeep.c
+++ batbeep.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
-
+#include <syslog.h>
 
 
 int main(int argc, char **  argv)
 {
+    
+    const char * log_ident = "batBeep";
     /* This part of the code creates the daemon */
+    /* Here we fork once so that the child is not the group leader */
     pid_t pID = fork();
     if(pID == -1)
     {
@@ -20,11 +25,51 @@ int main(int argc, char **  argv)
         exit(0);
     }
 
+    // Then we make the child a new session leader with no controlling terminal.
     if(setsid() == -1)
     {
         fprintf(stderr, "Failed to start a new session");
         return 1;
     }
+
+    /* Here we fork again so that we guarentee we can not reaquire a controlling 
+     * terminal */
+    switch (fork()) {
+    case -1: 
+            fprintf(stderr, "Failed to fork in the second place");
+            return -1;
+    case 0:
+            break;
+    default:
+            exit(0);
+    }
+    openlog(log_ident, LOG_CONS, LOG_DAEMON);
+
+    /* Clear the file mode creation mask */
+    umask(0);
+
+    /* Change the working directory to somewhere that 
+     * probably won't be unmounted while this program runs */
+    chdir("/sys/class/power_supply/BAT1");
+
+    /* Close the STD File Descriptors */ 
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    /* Open back up the 0, 1, 2 just in case somebody accidently 
+     * uses them */
+
+    int fd = open("/dev/null", O_RDWR);
+    if(fd != 0)
+    {
+        syslog(LOG_ERR, "Error FD did not match");
+        return 1;
+    }
+    if (dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO)
+        return 1;
+    if (dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO)
+        return 1;
 
 
     FILE * current_power, * full_power;
@@ -37,32 +82,32 @@ int main(int argc, char **  argv)
     {
         if(!(current_power = fopen("/sys/class/power_supply/BAT1/charge_now", "r")))
         {
-            fprintf(stderr, "Error in opening current_power file");
+            syslog(LOG_ERR, "Error in opening current_power file");
             return 1;
         }
 
         if(!(full_power = fopen("/sys/class/power_supply/BAT1/charge_full", "r")))
         {
-            fprintf(stderr, "Error in opening full_power file");
+            syslog(LOG_ERR, "Error in opening full_power file");
             return 1;
         }
 
         unsigned int current, full;
         if(!fscanf(current_power, "%u", &current))
         {
-            fprintf(stderr, "Error in reading current_power file");
+            syslog(LOG_ERR, "Error in reading current_power file");
             return 1;
         }
         
         if(!fscanf(full_power, "%u", &full))
         {
-            fprintf(stderr, "Error in reading full_power file");
+            syslog(LOG_ERR, "Error in reading full_power file");
             return 1;
         }
 
         if(current > full)
         {
-            fprintf(stderr, "Runtime error: Current charge greater than Max Capacity");
+            syslog(LOG_ERR, "Runtime error: Current charge greater than Max Capacity");
             return 1;
         } 
 #ifdef DEBUG
@@ -79,7 +124,8 @@ int main(int argc, char **  argv)
             system(temp_cmd);
         }
 
-        sleep(3);
+        syslog(LOG_DEBUG, "Still Working....");
+        sleep(1);
         fclose(full_power);
         fclose(current_power);
 
@@ -89,6 +135,6 @@ int main(int argc, char **  argv)
 
         
 
-
+    return 0;
 
 }
